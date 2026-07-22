c0414ea87297a3bbe668a323865667728ea232bc
Kevin John <143825085+kevin-github-code@users.noreply.github.com>
2026-07-22 12:24:46 +0200

    Resolvendo o terceiro exercicio

---
+++ exercicio3.js
@@ -0,0 +1,20 @@
+/*
+Exemplo: "o rato roeu a roupa" → "roupa a roeu rato o".
+ Não pode usar .reverse() diretamente no array de palavras
+  — inverta manualmente com um laço.
+*/
+
+let frase = "o rato roeu a roupa"
+let listaPalavras = []
+let fraseInvertida = []
+let palavraAtual = ""
+
+for (let i = 0; i < array.length; i++) {
+
+}
+
+
+for (let j = 4; j > 0; j--) {
+
+
+}
